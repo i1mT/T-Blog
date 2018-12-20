b09d15246ca7297d454a4dd1f96ae1cf286958d1
tfh93121 <tfhhh@qq.com>
2018-12-20 16:00:00 +0000

    更新CI

--- CI.sh
+++ CI.sh
@@ -6,30 +6,39 @@ deployPath="/home/wwwroot/iimt_blog/domain/wwwiimt.me/web"
 
 # 拉取代码
 pull () {
+    echo "git pull" >> $FILE
     git pull >> $FILE
 }
 
 # 安装依赖
 install_dependices () {
+    echo "cnpm i" >> $FILE
     cnpm i >> $FILE
 }
 
 # 打包
 build () {
+    echo "npm run build" >> $FILE
     npm run build >> $FILE
 }
 
 # 删除文件
 delete_file () {
+    echo "rm -rf ${deployPath}/static" >> $FILE
     rm -rf ${deployPath}/static >> $FILE
+    echo "rm -rf ${deployPath}/deploy" >> $FILE
     rm -rf ${deployPath}/deploy >> $FILE
+    echo "rm -rf ${deployPath}/others" >> $FILE
     rm -rf ${deployPath}/others >> $FILE
 }
 
 # 移动文件
 move_file () {
+    echo "mv ./dist/* ${deployPath}/" >> $FILE
     mv ./dist/* ${deployPath}/
+    echo "mv ./deploy ${deployPath}/" >> $FILE
     mv ./deploy ${deployPath}/
+    echo "mv ./others ${deployPath}/" >> $FILE
     mv ./others ${deployPath}/
 }
 
