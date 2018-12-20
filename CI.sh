a3a75f90c6de86ed27d29707f530f6f14ab582c9
tfh93121 <tfhhh@qq.com>
2018-12-20 12:24:06 +0800

    完成CI

--- CI.sh
+++ CI.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "DEPLOY START" >> $FILE
+
 datetime=$(date '+%Y-%m-%d %H:%M:%S')
 FILE="/home/www/deploy_log/iimt.me/"$(date '+%Y-%m-%d')"_log.txt"
+echo "DEPLOY START" >> $FILE
 echo $datetime >> $FILE
 
 # 拉取代码
@@ -14,16 +15,16 @@ echo "---------  GIT PULL END ------------" >> $FILE
 
 # 安装依赖
 echo "---------  NPM I  ------------" >> $FILE
-echo "npm i" >> $FILE
-cmd=`npm i`
+echo "cnpm i" >> $FILE
+cmd=`cnpm i`
 result=${cmd}
 echo "$result" >> $FILE
 echo "---------  NPM I END  ------------" >> $FILE
 
 # 打包
 echo "---------  NPM RUN BUILD  ------------" >> $FILE
-echo "npm run build" >> $FILE
-cmd=`npm run build`
+echo "cnpm run build" >> $FILE
+cmd=`cnpm run build`
 result=${cmd}
 echo "$result" >> $FILE
 echo "---------  NPM RUN BUILD END  ------------" >> $FILE
@@ -31,8 +32,8 @@ echo "---------  NPM RUN BUILD END  ------------" >> $FILE
 
 # 移动文件
 echo "---------  MOVE FILE  ------------" >> $FILE
-echo "mv ./dist /home/wwwroot/iimt_blog/domain/wwwiimt.me/web/" >> $FILE
-cmd=`mv ./dist /home/wwwroot/iimt_blog/domain/wwwiimt.me/web/`
+echo "rm -rf /home/wwwroot/iimt_blog/domain/wwwiimt.me/web/static/* &&  mv ./dist/* /home/wwwroot/iimt_blog/domain/wwwiimt.me/web/" >> $FILE
+cmd=`rm -rf /home/wwwroot/iimt_blog/domain/wwwiimt.me/web/static/* &&  mv ./dist/* /home/wwwroot/iimt_blog/domain/wwwiimt.me/web/`
 result=${cmd}
 echo "$result" >> $FILE
 echo "---------  MOVE FILE END  ------------" >> $FILE
