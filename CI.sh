8b34d9d8d4c122f15c8a5a24de19eda7b96bf56c
tfh93121 <tfhhh@qq.com>
2018-12-20 11:56:49 +0800

    修改CI

--- CI.sh
+++ CI.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Deploying... 1"
+echo "Deploying..."
+
+cmd=`git pull`
+
+datetime=$(date '+%Y-%m-%d %H:%M:%S')
+
+result=${cmd}
+
+FILE="/home/www/deploy_log/iimt.me/"$(date '+%Y-%m-%d')"_log.txt"
+
+
+echo $datetime >> $FILE
+echo "$aaa" >> $FILE
+echo "--------------------------------------------------------------------------------------------------------" >> $FILE
