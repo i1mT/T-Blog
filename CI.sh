a56cf0ee5050011d716e4733a16ebfb10b090aba
tfh93121 <tfhhh@qq.com>
2018-12-22 00:03:43 +0800

    CI wait

--- CI.sh
+++ CI.sh
@@ -8,56 +8,66 @@ deployPath="/home/wwwroot/iimt_blog/domain/wwwiimt.me/web"
 # 拉取
 pull () {
     echo "git pull" >> $FILE
-    git pull >> $FILE
+    output=`git pull`
+    echo "${output}" >> $FILE
 }
 
 # 安装依赖
 install_dependices () {
     echo "cnpm i" >> $FILE
-    cnpm i >> $FILE
+    output=`cnpm i`
+    echo "${output}" >> $FILE
 }
 
 # 打包
 build () {
     echo "npm run build" >> $FILE
-    npm run build >> $FILE
+    output=`npm run build`
+    echo "${output}" >> $FILE
 }
 
 # 更新博客程序
 update_src () {
     # 删除
     echo "rm -rf ${deployPath}/static" >> $FILE
-    rm -rf ${deployPath}/static >> $FILE
+    output=`rm -rf ${deployPath}/static`
+    echo "${output}" >> $FILE
     # 更新
     echo "cp -r ${PWD}/dist/* ${deployPath}/" >> $FILE
-    cp -r ${PWD}/dist/* ${deployPath}/
+    output=`cp -r ${PWD}/dist/* ${deployPath}/`
+    echo "${output}" >> $FILE
 }
 
 # 更新API
 update_API() {
     # 删除
     echo "rm -rf ${deployPath}/API" >> $FILE
-    rm -rf ${deployPath}/API >> $FILE
+    output=`rm -rf ${deployPath}/API`
+    echo "${output}" >> $FILE
     # 更新
     echo "cp -r ${PWD}/API ${deployPath}/" >> $FILE
-    cp -r ${PWD}/API ${deployPath}/
+    output=`cp -r ${PWD}/API ${deployPath}/`
+    echo "${output}" >> $FILE
 }
 
 # 更新其他页面文件
 update_others () {
     # 删除
     echo "rm -rf ${deployPath}/others" >> $FILE
-    rm -rf ${deployPath}/others >> $FILE
+    output=`rm -rf ${deployPath}/others`
+    echo "${output}" >> $FILE
     # 更新
     echo "cp -r ${PWD}/others ${deployPath}/" >> $FILE
-    cp -r ${PWD}/others ${deployPath}/
+    output=`cp -r ${PWD}/others ${deployPath}/`
+    echo "${output}" >> $FILE
 }
 
 # 删除临时文件
 delete_dist () {
     # 删除
     echo "rm -rf ${PWD}/dist" >> $FILE
-    rm -rf ${PWD}/dist >> $FILE
+    output=`rm -rf ${PWD}/dist`
+    echo "${output}" >> $FILE
 }
 
 echo "---------------    DEPLOY START @$datetime   --------------------------------------" >> $FILE
